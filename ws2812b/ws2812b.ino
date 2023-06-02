b6db3fc41bc2fc69f635c2c2894009f4f5deab09
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2023-06-02 22:36:09 +0800

    WS2812B - 滚动文字

--- ws2812b/ws2812b.ino
+++ ws2812b/ws2812b.ino
@@ -1,29 +1,57 @@
-#include <FastLED.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_NeoMatrix.h>
+#include <Adafruit_NeoPixel.h>
 
-#define NUM_LEDS 256
-#define LED_TYPE WS2812B
-#define LED_PIN 2
-#define COLOR_ORDER GRB
+#define PIN 2
 
-uint8_t max_bright = 5;
+// MATRIX DECLARATION:
+// Parameter 1 = width of the matrix
+// Parameter 2 = height of the matrix
+// Parameter 3 = pin number (most are valid)
+// Parameter 4 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the matrix; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
+//     rows or in vertical columns, respectively; pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
+//     in the same order, or alternate lines reverse direction; pick one.
+//   See example below for these values in action.
+// Parameter 5 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_GRBW    Pixels are wired for GRBW bitstream (RGB+W NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 
-CRGB leds[NUM_LEDS];
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(32, 8, PIN,
+                                               NEO_MATRIX_TOP + NEO_MATRIX_LEFT + NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG,
+                                               NEO_GRB + NEO_KHZ800);
 
-uint8_t beginHue = 0;
-uint8_t deltaHue = 1;
+const uint16_t colors[] = {
+  matrix.Color(0, 121, 255), matrix.Color(0, 223, 162), matrix.Color(246, 250, 112),
+  matrix.Color(255, 0, 96)
+};
 
 void setup() {
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
-  FastLED.setBrightness(max_bright);
-
-  for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB::Green;
-  }
-  FastLED.show();
-  delay(1000);
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(10);
+  matrix.setTextColor(colors[0]);
 }
 
+int x = matrix.width();
+int pass = 0;
+
 void loop() {
-  fill_rainbow(leds, NUM_LEDS, beginHue, random8(10));
-  FastLED.show();
+  matrix.fillScreen(0);
+  matrix.setCursor(x, 0);
+  matrix.print(F("Hello World"));
+  if (--x < -36) {
+    x = matrix.width();
+    if (++pass >= 3) pass = 0;
+    matrix.setTextColor(colors[pass]);
+  }
+  matrix.show();
+  delay(100);
 }
